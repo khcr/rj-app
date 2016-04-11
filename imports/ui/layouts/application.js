d97f7deec8940c5cee27f77015cab222ce853685
khcr <kocher.ke@gmail.com>
2016-04-11 21:31:25 +0200

    design improvements

--- imports/ui/layouts/application.js
+++ imports/ui/layouts/application.js
@@ -1,1 +1,13 @@
+import { Template } from 'meteor/templating';
+
 import './application.html'
+
+Template.ApplicationLayout.helpers({
+  attributes(route) {
+    let className;
+    if (Router.current().route.path() === route) { className = "current"; }
+    return {
+      class: className
+    };
+  }
+});
