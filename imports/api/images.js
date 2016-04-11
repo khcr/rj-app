4c268428d54e1e82ae5b07d412eefcdbe622b099
khcr <kocher.ke@gmail.com>
2016-04-11 23:03:47 +0200

    fixes

--- imports/api/images.js
+++ imports/api/images.js
@@ -4,10 +4,10 @@ import { Meteor } from 'meteor/meteor';
 export const Images = new FS.Collection("images", {
   stores: [
     new FS.Store.GridFS("images", { transformWrite: function(fileObj, readStream, writeStream) {
-      gm(readStream, fileObj.name()).resize('200>').stream().pipe(writeStream);
+      gm(readStream, fileObj.name()).resize('1000>').stream().pipe(writeStream);
     }}),
     new FS.Store.GridFS("thumbs", { transformWrite: function(fileObj, readStream, writeStream) {
-      gm(readStream, fileObj.name()).resize('50>').stream().pipe(writeStream);
+      gm(readStream, fileObj.name()).resize('130>').stream().pipe(writeStream);
     }}),
   ],
   filter: {
