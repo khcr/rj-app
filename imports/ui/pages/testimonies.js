4c1edc16e9c0db43e0d59d855b136d4c4388b9ee
khcr <kocher.ke@gmail.com>
2016-04-23 15:31:31 +0200

    database connection

--- imports/ui/pages/testimonies.js
+++ imports/ui/pages/testimonies.js
@@ -1,7 +1,15 @@
+import { Testimonies } from '../../api/testimonies.js';
+
 import './testimonies.html';
 
 import { Template } from 'meteor/templating';
 
 Template.Testimonies.onCreated(function TestimoniesOnCreated() {
   Meteor.subscribe('testimonies');
+});
+
+Template.Testimonies.helpers({
+  testimonies() {
+    return Testimonies.find();
+  },
 });
