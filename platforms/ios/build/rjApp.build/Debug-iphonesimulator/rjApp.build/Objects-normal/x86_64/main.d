a73875d6ef580d65c4d9e60b65258f351438aceb
Keran Kocher <kocher.ke@gmail.com>
2017-03-06 23:48:23 +0100

    remove unnecessary build directory

--- platforms/ios/build/rjApp.build/Debug-iphonesimulator/rjApp.build/Objects-normal/x86_64/main.d
+++
@@ -1,8 +0,0 @@
-dependencies: \
-  /Users/khcr/Projets/Github/rjApp/platforms/ios/internal/main.m \
-  /Users/khcr/Projets/Github/rjApp/platforms/ios/internal/NativeScript.framework/Headers/NativeScript.h \
-  /Users/khcr/Projets/Github/rjApp/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Diagnostics.h \
-  /Users/khcr/Projets/Github/rjApp/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime.h \
-  /Users/khcr/Projets/Github/rjApp/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Inspector.h \
-  /Users/khcr/Projets/Github/rjApp/platforms/ios/internal/TNSExceptionHandler.h \
-  /Users/khcr/Projets/Github/rjApp/platforms/ios/internal/TNSDebugging.h
--- platforms/ios/rjApp/app/app.js
+++
@@ -1,8 +0,0 @@
-var application = require("application");
-
-application.start({ moduleName: "views/feed/feed" });
-
-/*
-Do not place any code after the application has been started as it will not
-be executed on iOS.
-*/
